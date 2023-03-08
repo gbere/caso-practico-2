b1a4f33b15cdee189ee662bff7d3938ac506ed1d
Gerard Berengué <gerard@bere.cat>
2023-03-08 19:12:30 +0100

    update container registry credentials

--- terraform/output-files.tf
+++ terraform/output-files.tf
@@ -1,9 +1,10 @@
 # Creamos un archivo de variables para ansible que contiene las credenciales para
-# el Azure container registry
+# el Azure container registry con permisos de 0700
 resource "local_sensitive_file" "acr" {
-  filename = "${path.module}/../ansible/vars/azurerm_container_registry_vars.yaml"
+  filename = "${path.module}/../ansible/vars/azure_container_registry_vars.yaml"
   content = yamlencode({
-    acr_login_server : "${azurerm_container_registry.main.login_server}",
-    acr_password : "${azurerm_container_registry.main.admin_password}",
+    cr_registry : "${azurerm_container_registry.main.login_server}",
+    cr_username : "${azurerm_container_registry.main.admin_username}",
+    cr_password : "${azurerm_container_registry.main.admin_password}",
   })
 }
