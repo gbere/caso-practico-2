194fcabde949e3d33d745975652db18166456aa2
Gerard Berengué <gerard@bere.cat>
2023-03-08 19:34:01 +0100

    generate hosts

--- terraform/output-files.tf
+++ terraform/output-files.tf
@@ -3,8 +3,18 @@
 resource "local_sensitive_file" "acr" {
   filename = "${path.module}/../ansible/vars/azure_container_registry_vars.yaml"
   content = yamlencode({
-    cr_registry : "${azurerm_container_registry.main.login_server}",
-    cr_username : "${azurerm_container_registry.main.admin_username}",
-    cr_password : "${azurerm_container_registry.main.admin_password}",
+    "cr_registry" : "${azurerm_container_registry.main.login_server}",
+    "cr_username" : "${azurerm_container_registry.main.admin_username}",
+    "cr_password" : "${azurerm_container_registry.main.admin_password}",
+  })
+}
+
+# Creamos el archivo hosts.yaml para ansible
+resource "local_file" "hosts" {
+  filename = "${path.module}/../ansible/hosts.yaml"
+  content = yamlencode({
+    "vm1" : {
+      "hosts" : "${azurerm_linux_virtual_machine.vm1.public_ip_address}",
+    },
   })
 }
