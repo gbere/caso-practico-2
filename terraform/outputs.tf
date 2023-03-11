9866634da9fcce9bd325a26dcc19995a4b79675f
Gerard Berengué <gerard@bere.cat>
2023-03-11 11:47:28 +0100

    vm1 playbook

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "container_registry_login_server" {
   value = azurerm_container_registry.main.login_server
 }
 
+output "container_registry_admin_username" {
+  value = azurerm_container_registry.main.admin_username
+}
+
 output "container_registry_admin_password" {
   value     = azurerm_container_registry.main.admin_password
   sensitive = true
