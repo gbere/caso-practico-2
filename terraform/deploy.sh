39ec0e260e2d0898ac6777b3dc16476f887fc62a
Gerard Berengué <gerard@bere.cat>
2023-03-11 11:51:05 +0100

    add chdir on terraform deploy by script dir

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Generar par de claves SSH en el nodo de control.
-ssh-keygen -m PEM -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa_cp2 <<< y
+ssh-keygen -m PEM -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa_cp2
 
 # Inicialice un directorio de trabajo de Terraform nuevo o existente creando
 # archivos iniciales, cargar cualquier estado remoto, descargar módulos, etc.
-terraform init
+terraform -chdir=$SCRIPT_DIR init
 
 # Reescribe todos los archivos de configuración de Terraform a un formato canónico. Ambos
 # los archivos de configuración (.tf) y los archivos de variables (.tfvars) se actualizan.
-terraform fmt
+terraform -chdir=$SCRIPT_DIR fmt
 
 # Validar los archivos de configuración.
-terraform validate
+terraform -chdir=$SCRIPT_DIR validate
 
 # Genera un plan de ejecución especulativo, mostrando qué acciones Terraform
 # debería aplicar en la configuración actual.
-terraform plan -out=tfplan
+terraform -chdir=$SCRIPT_DIR plan -out=tfplan
 
 # Crea o actualiza la infraestructura según los archivos de configuración
 # de Terraform en el directorio actual.
-terraform apply -auto-approve "tfplan"
+terraform -chdir=$SCRIPT_DIR apply -auto-approve "tfplan"
