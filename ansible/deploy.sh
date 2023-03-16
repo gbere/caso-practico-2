401e93bec63803431acb841c1500ed21c71e9da0
Gerard Berengué <gerard@bere.cat>
2023-03-16 19:55:07 +0100

    wip - ansible aks1 deployment

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -4,3 +4,5 @@ ansible-galaxy collection install containers.podman
 
 # Ejecuta playbook playbook-vm1.yaml usando la clave privada ssh ~/.ssh/id_rsa_cp2
 ansible-playbook -i hosts.yaml playbook-vm1.yaml --private-key ~/.ssh/id_rsa_cp2
+
+ansible-playbook -i hosts.yaml playbook-aks1.yaml 
