27962c133ea23c6d766d277bd066fc460648d7e7
Gerard Berengué <gerard@bere.cat>
2023-03-20 18:43:00 +0100

    remove unused outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,22 +1,5 @@
-output "container_registry_login_server" {
-  value = azurerm_container_registry.main.login_server
-}
-
-output "container_registry_admin_username" {
-  value = azurerm_container_registry.main.admin_username
-}
-
-output "container_registry_admin_password" {
-  value     = azurerm_container_registry.main.admin_password
-  sensitive = true
-}
-
 output "vm1_pip" {
   value = azurerm_linux_virtual_machine.vm1.public_ip_address
-}
-
-output "akc_fqdn" {
-  value = azurerm_kubernetes_cluster.main.fqdn
 }
 
 output "aks1_pip" {
